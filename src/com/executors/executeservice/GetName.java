d4bd8125ec38b0717be842e769be50a434d7ffda
iddqd2d <iddqd2d@gmail.com>
2019-03-20 20:07:08 +0200

    fixed last

--- src/com/executors/executeservice/GetName.java
+++ src/com/executors/executeservice/GetName.java
@@ -8,17 +8,16 @@ import java.util.concurrent.Callable;
 
 public class GetName implements Callable<String> {
     private Connection connect;
-    private PreparedStatement preparedStatement;
     private int id;
 
-    GetName(int id) throws Exception{
+    public GetName(int id) throws Exception{
         connect = DriverManager.getConnection("jdbc:mysql://localhost/nameBD?characterEncoding=utf8", "login", "password");
         this.id = id;
     }
 
     @Override
     public String call() throws Exception {
-        preparedStatement = connect.prepareStatement("SELECT `name` FROM `users` WHERE `id` = ?");
+        PreparedStatement preparedStatement = connect.prepareStatement("SELECT `name` FROM `users` WHERE `id` = ?");
         preparedStatement.setInt(1, id);
         ResultSet result = preparedStatement.executeQuery();
         return (result.next())? result.getString(1): null;
